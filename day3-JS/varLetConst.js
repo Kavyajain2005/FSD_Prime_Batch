99a93f6fdd92699dd90a1e0cf491f340f704bd27
Kavyajain2005 <jainkavya200500@gmail.com>
2025-02-17 15:16:49 +0530

    JS Work

--- day3-JS/varLetConst.js
+++ day3-JS/varLetConst.js
@@ -2,12 +2,33 @@
 // Let => Block Scope
 // Const => Block Scope
 
-const temp = () => {
-    if(true) {
-        var username = "Kavya";
-    }
-    console.log(username);
+// const temp = () => {
+//     if(true) {
+//         var username = "Kavya";
+//     }
+//     console.log(username);
+// };
+
+// temp();
+// console.log(username);
+
+// function sumMod10(){
+//     function printText(a, b) {
+//         return (a + b) % 10;
+//     }
+//     return printText(10, 20);
+// }
+
+// console.log(sumMod10());
+
+const sumMod10 = (a, b) => {
+    const ans = (a + b) % 10;
+    
+    const printPretty = (txt) => {
+        console.log("===", txt, "====");
+    };
+    
+    printPretty(ans);
 };
 
-temp();
-console.log(username);
+sumMod10(10, 34);
