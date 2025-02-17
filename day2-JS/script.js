8fb5d0023392a3a286c36d3b40217e10c24bb0af
Kavyajain2005 <jainkavya200500@gmail.com>
2025-02-17 15:00:20 +0530

    JS Work

---
+++ day2-JS/script.js
@@ -0,0 +1,48 @@
+// // let const var
+// var a = "Hello";
+// var b = "Hello";
+// var c = 10;
+// var d = true;
+// var e = "10";
+
+// // Loose Equality
+// if(a == b) {
+//     console.log("Yes");
+// }
+// else {
+//     console.log("No");
+// }
+
+// // Strict Equality
+// console.log(c === e);
+
+// if(b == c) {
+//     console.log("Yes");
+// }
+// else {
+//     console.log("No");
+// }
+
+// if(b == d) {
+//     console.log("Yes");
+// }
+// else {
+//     console.log("No");
+// }
+
+// if(c == d) {
+//     console.log("Yes");
+// }
+// else {
+//     console.log("No");
+// }
+
+console.log(a);
+var a = "Hello";
+
+console.log(a);
+var a = "Hi";
+
+console.log(a);
+console.log(abc);
+
